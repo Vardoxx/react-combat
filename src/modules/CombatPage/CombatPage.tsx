7a71a7a018943e6350308d6b1a456703a0b86aa1
Maks <maksgavrilov956@gmail.com>
2024-08-04 21:50:48 +0300

    main logic for balanse is done

--- src/modules/CombatPage/CombatPage.tsx
+++ src/modules/CombatPage/CombatPage.tsx
@@ -1,12 +1,23 @@
 import reactLogo from "./img/react.svg";
 import s from "./CombatPage.module.scss";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../store/store";
+import { increment } from "../../store/sliced/incrementToClick";
 
 const CombatPageModule = () => {
+  useSelector((state: RootState) => state.clickToEarn.value);
+  const dispatch = useDispatch();
+
   return (
     <>
       <div className={s.logo_container}>
         {/* {tapValue && <p className="value_output">+10</p>} */}
-        <img src={reactLogo} className={s.logo} alt="React logo" />
+        <img
+          onClick={() => dispatch(increment())}
+          src={reactLogo}
+          className={s.logo}
+          alt="React logo"
+        />
       </div>
     </>
   );
